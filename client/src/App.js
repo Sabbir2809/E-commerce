7900e90179d91c3a95c4a3c428015eb6a26d5c67
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-06-16 23:39:51 +0600

    Features: Login & Signup Pages, React Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
